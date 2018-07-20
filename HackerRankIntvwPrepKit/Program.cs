173e1404d4679142e2221c688fc092bfc45b2393
mikespriggs26 <mikespriggs@msn.com>
2018-07-19 20:33:30 -0400

    Completed his challenge

--- HackerRankIntvwPrepKit/Program.cs
+++ HackerRankIntvwPrepKit/Program.cs
@@ -10,42 +10,72 @@ namespace HackerRankIntvwPrepKit
     {
         static void Main(string[] args)
         {
-            int[] myArray = new int[] { 2, 5, 1, 3, 4 };
-            int[] myArray1 = new int[] { 2, 1, 5, 3, 4 };
-            int[] myArray2 = new int[] { 1, 2, 5, 3, 7, 8, 6, 4 };
-            int[] myArray3 = new int[] { 5, 1, 2, 3, 7, 8, 6, 4 };
+            //***************Birthday Cake Candles  SUCCESS!!!
+            int[] myArray = new int[] { 3, 2, 1, 3 };
+            int tallestCandle = 0;
             int counter = 0;
-            for (int i = 0; i < myArray2.Length; i++)
+            
+            for (int i = 0; i < myArray.Length; i++)
             {
-                int sum = myArray2[i] - 1 - i;//measures displacement
-                if (sum == 1)
+                if(myArray[i] > tallestCandle)
                 {
-                    counter++;
-                    //Console.WriteLine(counter);
-                }
-                else if (sum == 2)
-                {
-                    counter += 2;
-                    //Console.WriteLine(counter);
-                }
-                else if (sum <= 0)
-                {
-                    //Console.WriteLine(counter);
-                    continue;
+                    tallestCandle = myArray[i];
                 }
                 else
                 {
-                    counter = 0;
-                    Console.WriteLine("Too chaotic");
-                    break;
-
+                    continue;
                 }
 
             }
-            if (counter > 0)
+            foreach (var item in myArray)
             {
-                Console.WriteLine(counter);
+                if(item == tallestCandle)
+                {
+                    counter++;
+                }
             }
+            Console.WriteLine(counter);
+
+
+
+
+            //**************************New Year Chaos UNSOLVED
+            //int[] myArray = new int[] { 2, 5, 1, 3, 4 };
+            //int[] myArray1 = new int[] { 2, 1, 5, 3, 4 };
+            //int[] myArray2 = new int[] { 1, 2, 5, 3, 7, 8, 6, 4 };
+            //int[] myArray3 = new int[] { 5, 1, 2, 3, 7, 8, 6, 4 };
+            //int counter = 0;
+            //for (int i = 0; i < myArray2.Length; i++)
+            //{
+            //    int sum = myArray2[i] - 1 - i;//measures displacement
+            //    if (sum == 1)
+            //    {
+            //        counter++;
+            //        //Console.WriteLine(counter);
+            //    }
+            //    else if (sum == 2)
+            //    {
+            //        counter += 2;
+            //        //Console.WriteLine(counter);
+            //    }
+            //    else if (sum <= 0)
+            //    {
+            //        //Console.WriteLine(counter);
+            //        continue;
+            //    }
+            //    else
+            //    {
+            //        counter = 0;
+            //        Console.WriteLine("Too chaotic");
+            //        break;
+
+            //    }
+
+            //}
+            //if (counter > 0)
+            //{
+            //    Console.WriteLine(counter);
+            //}
 
             //Console.ReadKey();
             //************************************************
